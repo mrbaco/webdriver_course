e23db217b20f6d35d0a06e7f9078f08497baeacf
do.sokolov <do.sokolov@severstal.com>
2020-07-23 01:38:02 +0300

    task 14

---
+++ webdriver/src/test/java/selenium/TestBase.java
@@ -0,0 +1,74 @@
+package selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    private final String browser = "chrome";
+
+    long pageLoadTimeout = 10;
+    long implicitlyWait = 10;
+
+    @Before
+    public void start() {
+        switch (browser) {
+            case ("chrome"):
+                driver = new ChromeDriver();
+                break;
+            case ("firefox"):
+                driver = new FirefoxDriver();
+                break;
+            case ("edge"):
+                driver = new EdgeDriver();
+                break;
+        }
+
+        wait = new WebDriverWait(driver, 10);
+
+        driver.manage().timeouts().pageLoadTimeout(pageLoadTimeout, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(implicitlyWait, TimeUnit.SECONDS);
+    }
+
+    public void waitReadyStateToComplete() {
+        wait.until(driver -> ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete"));
+    }
+
+    public void loginToAdminSection() {
+        driver.get("http://localhost/admin/");
+
+        // ожидание загрузки страницы
+        waitReadyStateToComplete();
+
+        // вход в админ-панель
+        driver.findElement(By.cssSelector("input[name='username']"))
+                .sendKeys("admin");
+        driver.findElement(By.cssSelector("input[name='password']"))
+                .sendKeys("admin");
+
+        driver.findElement(By.cssSelector("button[name='login']"))
+                .click();
+
+        // ожидание загрузки страницы
+        waitReadyStateToComplete();
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+}
---
+++ webdriver/src/test/java/selenium/task14.java
@@ -0,0 +1,65 @@
+package selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.numberOfWindowsToBe;
+
+public class task14 extends TestBase {
+
+    @Test
+    public void test1() {
+        // перейти в админ-панель
+        loginToAdminSection();
+
+        // перейти в раздел Countries и дождаться загрузки страницы
+        By countriesLink = By.cssSelector("#box-apps-menu li a[href*='countries']");
+
+        wait.until(elementToBeClickable(countriesLink));
+        driver.findElement(countriesLink).click();
+
+        waitReadyStateToComplete();
+
+        // нажать на первую страну в списке и дождаться загрузки страницы
+        By countryLink = By.cssSelector("[name='countries_form'] tr.row td:nth-child(5) a");
+
+        wait.until(elementToBeClickable(countryLink));
+        driver.findElement(countryLink).click();
+
+        waitReadyStateToComplete();
+
+        // получить handle текущего окна
+        String mainWindowHandle = driver.getWindowHandle();
+
+        // получить список ссылок на страницы, открывающиеся в новом окне
+        List<WebElement> externalLinks = driver.findElements(By.xpath("//*[contains(@class, 'fa-external-link')]/.."));
+        for (WebElement link : externalLinks) {
+            // нажать на ссылку
+            link.click();
+
+            // дождаться пока откроется новое окно
+            wait.until(numberOfWindowsToBe(2));
+
+            // найти handle нового окна
+            Set<String> windowHandles = driver.getWindowHandles();
+            for (String handle : windowHandles) {
+                if (handle.equals(mainWindowHandle)) continue;
+
+                // переключиться на новое окно
+                driver.switchTo().window(handle);
+
+                // закрыть открытое окно
+                driver.close();
+
+                // переключиться на главное окно
+                driver.switchTo().window(mainWindowHandle);
+            }
+        }
+    }
+
+}
